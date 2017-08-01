9789f7a30df001d02f75578e94d15f76a2d9e44c
Rick van de Loo <rickvandeloo@gmail.com>
2017-08-01 15:25:17 +0200

    add http to base url

--- deploy_production.sh
+++ deploy_production.sh
@@ -6,7 +6,7 @@ PRODUCTION_HOSTNAME='example.hypernode.io'
 set -e 
 
 # apply deployment playbook with production settings
-ansible-playbook provisioning/magento1.yml --extra-vars "@vars_prod.yml" --user=app -i "$PRODUCTION_HOSTNAME," # mind the trailing comma
+ansible-playbook provisioning/magento1.yml --verbose --extra-vars "@vars_prod.yml" --user=app -i "$PRODUCTION_HOSTNAME," # mind the trailing comma
 
 # test if the production Hypernode was succesfully provisioned
 TEST_URL=$PRODUCTION_HOSTNAME nosetests testcase.py
